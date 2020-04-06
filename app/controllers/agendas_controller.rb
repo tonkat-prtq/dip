de06912910605e5c64d4a16459983418917f3938
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-04-06 22:12:15 +0900

    #6 Add:基本的なagenda削除機能とボタンの実装

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  # before_action :set_agenda, only: %i[show edit update destroy]
+  before_action :set_agenda, only: %i[destroy]
 
   def index
     @agendas = Agenda.all
@@ -19,6 +19,11 @@ class AgendasController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @agenda.destroy
+    redirect_to dashboard_url, notice: "Agendaを削除しました"
   end
 
   private
